435c3f9ce6e2123879659d13096307d877eec9ab
Fernando Luiz Parisotto <fernando.parisotto@gmail.com>
2023-03-01 20:06:34 -0300

    fix endpoint ref in spa

--- web-spa/src/api/mod.rs
+++ web-spa/src/api/mod.rs
@@ -3,4 +3,4 @@ pub mod bookmarks_api;
 pub mod search_api;
 pub mod tags_api;
 
-pub const PUBLIC_API_ENDPOINT: &str = include_str!("public_api_endpoint.txt");
+pub const PUBLIC_API_ENDPOINT: &'static str = std::env!("PUBLIC_API_ENDPOINT");
